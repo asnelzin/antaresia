c03816e819d03c4d7f2a5be3fe8d28917a672996
asnelzin <asnelzin@gmail.com>
2014-12-01 18:29:37 +0300

    Refactor models, add docopt

--- antaresia/core.py
+++ antaresia/core.py
@@ -1,51 +1,25 @@
-import socket
-import sys
+"""
 
-from antaresia.models import Request, serve_static
+Usage: antaresia [options] <path>
+       antaresia -h | --help
+       antaresia -v | --version
 
-HOST = '127.0.0.1'
-MAX_CONNECTIONS = 100
-BUFLEN = 1024
+Options:
+  -h, --help            Show this screen.
+  -v, --version         Show version.
+  -p PORT, --port=PORT  Port that will be used [default: 5000].
 
+"""
 
-def read_data_from_socket(sock):
-    data = b''
-    while True:
-        data += sock.recv(BUFLEN)
-        if b'\r\n\r\n' in data:
-            break
-    return data.decode('ascii')
+from docopt import docopt
 
-
-def run_server(port, directory):
-    print('Server a server on http://{0}:{1}/\n'.format(HOST, port))
-
-    addr = (HOST, port)
-    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serversock.bind(addr)
-    serversock.listen(MAX_CONNECTIONS)
-
-    while True:
-        client, addr = serversock.accept()
-        print('Connection from: ', addr)
-
-        data = read_data_from_socket(client)
-        print(data)
-
-        request = Request(data)
-        head, body = serve_static(request=request, directory=directory)
-        print(head.decode('ascii'))
-        client.sendall(head + body)
-
-        client.close()
+from antaresia.models import Server
 
 
 def main():
-    if len(sys.argv) != 3:
-        print('Usage: {0} PORT DIR'.format(sys.argv[0]))
-        sys.exit(1)
-    else:
-        port = int(sys.argv[1])
-        directory = sys.argv[2]
-        run_server(port, directory)
+    args = docopt(__doc__, version='antaresia v0.1')
+
+    port = int(args['--port'])
+    directory = args['<path>']
+    server = Server(port=port)
+    server.run(directory)
--- antaresia/models.py
+++ antaresia/models.py
@@ -1,9 +1,11 @@
 import mimetypes
 import os.path
 import re
+import socket
 import sys
 import urllib.parse
 
+from antaresia import settings
 from antaresia.utils import render_response, http_404
 
 
@@ -20,45 +22,76 @@ class Request(object):
                 self.headers[key] = value
 
 
-def send_file(request, path):
-    data = open(path, 'rb').read()
-    mimetype = mimetypes.guess_type(path)[0]
+class Server(object):
+    def __init__(self, host=settings.HOST, port=5000):
+        self.addr = (host, port)
+        self.serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serversock.bind(self.addr)
+        self.serversock.listen(settings.MAX_CONNECTIONS)
 
-    if mimetype is None:
-        mimetype = 'octet/stream'
+    def read_data_from_socket(self, sock):
+        data = b''
+        while True:
+            data += sock.recv(settings.BUFLEN)
+            if b'\r\n\r\n' in data:
+                break
+        return data.decode('ascii')
 
-    return render_response(code=200, comment='OK', mimetype=mimetype,
-                           body=data)
+    def send_file(self, request, path):
+        data = open(path, 'rb').read()
+        mimetype = mimetypes.guess_type(path)[0]
 
+        if mimetype is None:
+            mimetype = 'octet/stream'
 
-def send_directory(request, path):
-    files = [(not os.path.isdir(os.path.join(path, name)), name)
-             for name in os.listdir(path)]
-    files.sort()
+        return render_response(code=200, comment='OK', mimetype=mimetype,
+                               body=data)
 
-    files = [filename + ('' if isfile else '/') for isfile, filename in files]
+    def send_directory(self, request, path):
+        files = [(not os.path.isdir(os.path.join(path, name)), name)
+                 for name in os.listdir(path)]
+        files.sort()
 
-    encoding_line = '<meta charset="{encoding}">'
-    file_line = '<div><a href="{filename}">{filename}</a></div>'
-    data = (
-        [encoding_line.format(encoding=sys.getfilesystemencoding())] +
-        [file_line.format(filename=filename) for filename in files]
-    )
-    return render_response(code=200, comment='OK', mimetype='text/html',
-                           body='\n'.join(data).encode(sys.getfilesystemencoding()))
+        files = [filename + ('' if isfile else '/') for isfile, filename in files]
 
+        encoding_line = '<meta charset="{encoding}">'
+        file_line = '<div><a href="{filename}">{filename}</a></div>'
+        data = (
+            [encoding_line.format(encoding=sys.getfilesystemencoding())] +
+            [file_line.format(filename=filename) for filename in files]
+        )
+        return render_response(code=200, comment='OK', mimetype='text/html',
+                               body='\n'.join(data).encode(sys.getfilesystemencoding()))
 
-def serve_static(request, directory):
-    path = os.path.join(directory, request.path)
-    if not os.path.exists(path):
-        return http_404(request=request,
-                        message='Path {0} doesn\'t exist.'.format(path))
-    else:
-        if os.path.isdir(path):
-            index = os.path.join(path, 'index.html')
-            if os.path.exists(index):
-                return send_file(request, index)
-            else:
-                return send_directory(request, path)
+    def serve_static(self, request, directory):
+        path = os.path.join(directory, request.path)
+        if not os.path.exists(path):
+            return http_404(request=request,
+                            message='Path {0} doesn\'t exist.'.format(path))
         else:
-            return send_file(request, path)
+            if os.path.isdir(path):
+                index = os.path.join(path, 'index.html')
+                if os.path.exists(index):
+                    return self.send_file(request, index)
+                else:
+                    return self.send_directory(request, path)
+            else:
+                return self.send_file(request, path)
+
+    def run(self, directory):
+        print('Server a server on http://{0}:{1}/\n'.format(*self.addr))
+
+        while True:
+            client, addr = self.serversock.accept()
+            print('Connection from: ', addr)
+
+            data = self.read_data_from_socket(client)
+            print(data)
+
+            request = Request(data)
+            head, body = self.serve_static(request=request, directory=directory)
+            print(head.decode('ascii'))
+            client.sendall(head + body)
+
+            client.close()
